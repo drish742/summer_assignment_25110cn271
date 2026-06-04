a6269546c2d52d99a0468c63f3046fed83aab9bd
drish742 <drishtisssaxena9@gmail.com>
2026-06-04 04:08:54 +0000

    q5 uploaded

--- day_2/sum_o_dig_o_num.c
+++ day_2/sum_o_dig_o_num.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 int main(){
+    int n , r;
+    int sum = 0 ;
+    printf("enter the number : ");
+    scanf("%d" , &n);
+    while (n){
+        r = n%10;
+        sum = sum + r;
+        n = n / 10;
+    }
+    printf("the sum of digits is %d" , sum);
     
     return 0;
 }
