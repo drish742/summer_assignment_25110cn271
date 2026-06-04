514e4fcf91f9932633fd33b5289b9f2a6d45763f
drish742 <drishtisssaxena9@gmail.com>
2026-06-04 08:27:12 +0000

    q13 addded

--- day_4/fibonacci.c
+++ day_4/fibonacci.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
 int main(){
+    int se , a , b , n;
+    printf("enter the no. of terms");
+    scanf("%d" , &n);
+    a = 0;
+    b = 1;
+    if (n==0){
+        printf("0");
+    }
+    else if(n == 2){
+        printf("0 , 1");
+    }
+    else{
+        printf("0 , 1 ,");
+        for (int i = 3; i <= n ; i++){
+        
+            se = a + b;
+            a = b;
+            b = se ;
+            printf("%d ," ,se);
+        }
+    }
     return 0 ;
 }
