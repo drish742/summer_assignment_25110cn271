24773cb3ad42d64b522d8ee468b611dd2736e3c9
drish742 <drishtisssaxena9@gmail.com>
2026-06-04 07:43:35 +0000

    q9 added

--- day_3/num_prime.c
+++ day_3/num_prime.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 int main(){
+    int n, r;
+    int flag = 0;
+    printf("enter the number:");
+    scanf("%d" , &n);
+    for (int i = 2 ; i < n ; i++){
+        r = n % i;
+        if (r == 0){
+            flag ++;
+            break;
+        }
+    }
+    if (flag == 0){
+        printf("the number is prime");
+    }
     
     return 0;
 }
