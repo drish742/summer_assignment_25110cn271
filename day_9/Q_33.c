8e7131eec428abbfd96e408150d8f3c9a9eec340
drish742 <drishtisssaxena9@gmail.com>
2026-06-11 12:17:57 +0000

    day 9 complete

--- day_9/Q_33.c
+++ day_9/Q_33.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
 int main(){
+    int n = 5 , i , j ;
+    for(i = 1 ; i <= 5 ; i++){
+        for(j = 5 ; j >= i ; j--){
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0 ;
 }
---
+++ day_9/q36.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){ 
+    int i , j ;
+    for(i = 1 ; i <= 5 ; i++){
+        if(i == 1 || i == 5){
+            for(j = 1 ; j <= 5 ; j++){
+                printf("*");
+            }
+            //printf("\n");
+        }
+        else{
+            printf("*");
+            for(int k = 2 ; k <=4 ; k++){
+                printf(" ");
+
+        }
+        printf("*");
+    }
+    printf("\n");
+    }
+
+    return 0 ;
+}
