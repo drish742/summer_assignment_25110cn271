01468710ce1ffb66f39fb048265e874361322119
drish742 <drishtisssaxena9@gmail.com>
2026-06-11 08:58:32 +0000

    q22 added

--- day_6/q22.c
+++ day_6/q22.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+int main(){
+    // binary to decimal
+    int n ;
+    printf("enter the number ");
+    scanf("%d" , &n);
+    int sum = 0 , s = 1 , m , r;
+    m = n ;
+    while(n){
+        r = n%10 ;
+        sum = sum + (r*s) ;
+        s = s*2 ;
+        n = n/10 ;
+    }
+    printf("the decimal number is %d " , sum);
+    
+    return 0 ;
+}
