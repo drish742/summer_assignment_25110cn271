663ccdca028eb8dd5dab4c5dbec51b12cc10485e
Vaishnavi Saxena <drishtisssaxena9@gmail.com>
2026-06-25 00:13:29 +0530

    improvised

--- day_21/q84.c
+++ day_21/q84.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
 int main(){
     //convert lowecase to uppercase
-    
+    char a[1000] ;
+    printf("enter the string ");
+    fgets(a , sizeof(a) , stdin);
+    int count = strlen(a) ;
+    int len = count;
+    int i = 0 ; 
+    while(a[i] != '\0'){
+        if(a[i] >= 'a' && a[i] <= 'z'){
+            a[i] = a[i] - 32;
+        }
+        i++;
+    }
+    printf("the string after coversion is ");
+    printf("%s" , a);
     return 0;
 }
